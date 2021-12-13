c887ee3c82fdbb7381efa2140f98ccdeca29dbbf
nzambello <nzambello@protonmail.com>
2021-12-13 20:17:10 +0100

    chore: add deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), VitePWA()],
+  base: "/walk-up-alarm/",
+  plugins: [react(), VitePWA()],
 });
