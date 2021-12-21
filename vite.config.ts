14412ea138f46e5d9eacf8143cb3f8e2960c1a54
nzambello <nzambello@protonmail.com>
2021-12-21 18:40:35 +0100

    fix: pwa images

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate",
+  includeAssets: [
+    "favicon.svg",
+    "favicon.ico",
+    "robots.txt",
+    "apple-touch-icon.png",
+  ],
+  manifest: {
+    name: "Walk-up alarm",
+    short_name: "Walk-up",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/walk-up-alarm/",
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-    }),
-  ],
+  plugins: [react(), VitePWA(pwaOptions)],
 });
